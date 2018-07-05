7da5b68d541dd2d65a2f00a5aff7ffc65de720b9
Jonas van Ineveld <jonas@makeityourown.nl>
2018-07-05 15:45:41 +0200

    also trigger upon new issues

--- lib/updates_notifier_issue_change_listener.rb
+++ lib/updates_notifier_issue_change_listener.rb
@@ -11,5 +11,12 @@ class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
       end
     end
   end
+  def controller_issues_new_after_save(context={})
+    if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
+      if context[:issue] and context[:journal]
+        UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
+      end
+    end
+  end
 end
 
