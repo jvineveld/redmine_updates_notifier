67265acf97febe50753e64e050097e4b71232757
Jonas van Ineveld <jonas@makeityourown.nl>
2018-07-16 14:20:04 +0200

    Made the request to API a-sync
    This to prevent long submit times for ticket / project updates. When in-sync, the update of a issue can take up to 30sec's

--- lib/updates_notifier_issue_change_listener.rb
+++ lib/updates_notifier_issue_change_listener.rb
@@ -7,14 +7,18 @@ class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
   def controller_issues_edit_after_save(context={})
     if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
       if context[:issue] and context[:journal]
-        UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
+        Thread.new {
+          UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
+        }.run
       end
     end
   end
   def controller_issues_new_after_save(context={})
     if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
       if context[:issue] and context[:journal]
-        UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
+        Thread.new {
+          UpdatesNotifier.send_issue_update(User.current, context[:issue].id, context[:journal])
+        }.run
       end
     end
   end
