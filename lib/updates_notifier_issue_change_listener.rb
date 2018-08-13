665e1b07c8cee0235ed222e510e93d849311dd62
Jonas van Ineveld <jonas@makeityourown.nl>
2018-08-13 10:48:30 +0200

    Update updates_notifier_issue_change_listener.rb

--- lib/updates_notifier_issue_change_listener.rb
+++ lib/updates_notifier_issue_change_listener.rb
@@ -15,6 +15,7 @@ class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
     end
   end
   def controller_issues_new_after_save(context={})
+    Rails.logger.error("### issue new safe after, hook is run!")
     if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
       if context[:issue] and context[:journal]
         Thread.new {
@@ -25,6 +26,7 @@ class UpdatesNotifierIssueChangeListener < Redmine::Hook::Listener
     end
   end
   def controller_projects_new_after_save(context={})
+    Rails.logger.error("### issue new project, hook is run!")
     if !Setting.plugin_redmine_updates_notifier[:ignore_api_changes] or !context[:params][:format] or 'xml' != context[:params][:format]
       if context[:project] and context[:journal]
         Thread.new {
